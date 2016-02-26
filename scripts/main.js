42a26430fa48f85879a0aa23e6e2dc6a49b91db3
unknown <jame0168@WT308-13.ottawa.ad.algonquincollege.com>
2016-02-26 11:13:30 -0500

    lklk

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         $(window).scroll(function() {
             var scroller = $(window).scrollTop();
             if (scroller < 600) {
-                maxHeight = 300;
+                maxHeight = 500;
                 $('.sl1').animate({
                     opacity:1,
                 },1500);
@@ -11,31 +11,32 @@ $(document).ready(function(){
                     opacity:1,
                 },1500);
             } else if (scroller < 900) {
-                maxHeight = 600;
+                maxHeight = 1100;
                 $('.sl2').animate({
                     opacity:1
                 },1500);
                 $('.sr2').animate({
                     opacity:1
                 },1500);
-            } else if (scroller < 1200) {
-                maxHeight = 900;
+            } else if (scroller < 1500) {
+                maxHeight = 1800;
                 $('.sl3').animate({
                     opacity:1
                 },1500);
                 $('.sr3').animate({
                     opacity:1
                 },1500);
-            } else if (scroller < 1500) {
-                maxHeight = 1200;
-                $('.sl4').animate({
-                    opacity:1
-                },1500);
-                $('.sr4').animate({
-                    opacity:1
-                },1500);
-            } else{
-                maxHeight: 2500;
+//            } else if (scroller < 1500) {
+//                maxHeight = 1200;
+//                $('.sl4').animate({
+//                    opacity:1
+//                },1500);
+//                $('.sr4').animate({
+//                    opacity:1
+//                },1500);
+//            } 
+            }else{
+                maxHeight: 3000;
             }
             $('.line').css({
                 'height': maxHeight + "px"
