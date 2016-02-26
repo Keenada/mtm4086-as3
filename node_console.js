4c867d993e7acf08d5a75f9845292a630d0fb9c0
eileenxue <xue00017@algonquinlive.com>
2016-02-26 08:36:30 -0500

    Added files via upload

---
+++ node_console.js
@@ -0,0 +1,21 @@
+console.log("Hello Everyone");
+
+
+
+/*console.error(new Error('This is not allowed'));*/
+
+
+
+
+/*
+
+console.log('hello world');
+  // Prints: hello world, to stdout
+console.log('hello %s', 'world');
+  // Prints: hello world, to stdout
+console.error(new Error('Whoops, something bad happened'));
+
+const name = 'Will Robinson';
+console.warn(`Danger ${name}! Danger!`);
+  // Prints: Danger Will Robinson! Danger!, to stderr
+*/
